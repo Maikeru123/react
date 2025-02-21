2e51138ece19792e39cdce88c72db4b6ff9113f5
Maikeru123 <michaelvelez112301@gmail.com>
2025-02-21 16:01:49 +0800

    done todo list

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import Nav from '@/components/Nav'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Nav from "@/components/Nav";
+import { Outlet } from "react-router-dom";
 
-const mainlayout = () => {
+const MainLayout = () => {
   return (
-    <div>    
+    <div className="container mx-auto flex justify-center flex-col items-center">
       <Nav />
       <Outlet />
     </div>
+  );
+};
 
-  )
-}
-
-export default mainlayout
+export default MainLayout;
