e10618888fbf7981aca34166b26109cb6ec29651
Maikeru123 <michaelvelez112301@gmail.com>
2025-02-21 12:08:06 +0800

    Added buttons click me

---
+++ src/components/ClickMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+const ClickMe = () => {
+  return (
+    <div>
+      <Button>Click Me!</Button>
+    </div>
+  );
+};
+
+export default ClickMe;
